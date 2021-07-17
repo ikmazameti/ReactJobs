427cf8de53c1d8bb2346c70d18dc2aa4741ae5aa
sentinelbot <ikmazameti@gmail.com>
2021-07-17 18:16:56 +0000

    profile screen

---
+++ components/Profile/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, Image, Pressable, TextInput } from "react-native";
+
+//components
+import styles from "./styles";
+
+const ProfileScreen = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+};
+
+export default ProfileScreen;
